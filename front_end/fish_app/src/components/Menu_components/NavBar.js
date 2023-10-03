9db9a1b19d0e3d294f0f4f20a8dc5a91c0b77c16
Jason Reinoso <jasonjreinoso@gmail.com>
2023-10-03 19:42:36 -0400

    did alot of work on home page, needs a fix on scroll wheel

--- front_end/fish_app/src/components/Menu_components/NavBar.js
+++ front_end/fish_app/src/components/Menu_components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { MenuItem } from './Menu_items'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom' 
 import "./Navstyles.css"
 function NavBar() {
   return (
--- front_end/fish_app/src/components/home_components/Home.js
+++ front_end/fish_app/src/components/home_components/Home.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import Menu from '../Menu_components/Menu'
 import NavBar from '../Menu_components/NavBar'
+import "./Homestyle.css"
+import fishy from './bluebit.png'
 function Home() {
   return (
     <div className='Homepage'>
 
         <NavBar/>
+        <div className='grid-container'>
+          <div className='Bluebit'>
+            <img className='fishy'  src={fishy} />
+          </div>
+          <div className='info'>
+            <div className='Motto'>For all of your fishing needs!</div>
+            <div className='ad'>Fishing in a unfamiliar state? View fishing regulations by state!</div>
+            <div><button className='fish button'>Fishing regulations</button></div>
+          </div>
+        </div>
     </div>
   )
 }
