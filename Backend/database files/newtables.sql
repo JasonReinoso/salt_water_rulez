e716cf9360e8583575ebd1637fc9b5ff8ea1f027
Jason Reinoso <jasonjreinoso@gmail.com>
2023-10-24 17:41:03 -0400

    added new sql files

---
+++ Backend/database files/newtables.sql
@@ -0,0 +1,37 @@
+create database salt_water_rulez;
+use salt_water_rulez;
+
+create table States(
+state_id varchar(2),
+state_name varchar(40),
+primary key(state_id)
+);
+
+create table Log(
+Id int auto_increment,
+picture varchar(40),
+released boolean,
+TypeofFish varchar(100),
+weight double,
+length double,
+tools varchar (300),
+FishMethod varchar(100),
+fecha datetime,
+username varchar(100),
+weather varchar(100),
+primary key(Id)
+);
+
+insert into States (state_id,state_name)
+values ("de","delaware");
+
+
+
+create table fish_regulations(
+  fish_species varchar(255),
+  size_limit varchar(255),
+  Daily_possession_Limit int(255),
+  open_season varchar(1000),
+  state_id varchar(2),
+  Foreign key(state_id) references States(state_id)
+  );
